37ddc9fc8545e816d2dc3cd7c650bbd227e1484a
Daniel Chen <chendaniely@gmail.com>
2020-10-15 00:39:17 -0400

    book render fixes (use new_session = TRUE)

---
+++ _common.R
@@ -0,0 +1,12 @@
+set.seed(42)
+
+knitr::opts_chunk$set(
+  #comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  out.width = "100%",
+  fig.align = 'center',
+  #fig.width = 6,
+  #fig.show = "hold"
+  fig.asp = 0.618  # 1 / phi
+)
