f68bcfb063f8ef877c560676442c161415d98d18
Daniel Chen <chendaniely@users.noreply.github.com>
2021-09-27 10:45:30 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,3 @@
-install.packages("tidyverse")
-install.packages("remotes")
+pkgs <- c("tidyverse", "remotes", "RSQLite", "janitor", "survival", "survminer", "tidycensus","tidymodels", "themis", "writexl", "ggthemes", "fs")
+install.packages(pkgs)
 remotes::install_github("higgi13425/medicaldata")
