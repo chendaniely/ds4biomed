ec507e5de129ccfc230b73265be6afaaecc9e889
Daniel Chen <chendaniely@users.noreply.github.com>
2021-09-27 23:48:58 -0400

    just install tidycensus to test with conda gdal

--- install.R
+++ install.R
@@ -1,3 +1,5 @@
-pkgs <- c("tidyverse", "remotes", "RSQLite", "janitor", "survival", "survminer", "tidycensus","tidymodels", "themis", "writexl", "ggthemes", "fs")
-install.packages(pkgs)
-remotes::install_github("higgi13425/medicaldata")
+#pkgs <- c("tidyverse", "remotes", "RSQLite", "janitor", "survival", "survminer", "tidycensus","tidymodels", "themis", "writexl", "ggthemes", "fs")
+#install.packages(pkgs)
+#remotes::install_github("higgi13425/medicaldata")
+
+install.packages("tidycensus")
